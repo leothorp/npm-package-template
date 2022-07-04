925217ee64dd4fe702fd49d8261741aa7f9beb13
leothorp <leothorp@users.noreply.github.com>
2022-07-04 13:35:56 -0500

    shell

--- bin/npm-package-template.js
+++ bin/npm-package-template.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-//can use a package like 'commander' for CLI flag handling
 import { main } from "../src";
+import sh from 'shelljs';
 
+// sh.mkdir('-p', `./`);
+console.log(process.argv)
 main()
