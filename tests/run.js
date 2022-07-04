02e345abb658e413a65fe8649c48642a2a537126
leothorp <leothorp@users.noreply.github.com>
2022-07-04 14:52:32 -0500

    new

--- tests/run.js
+++ tests/run.js
@@ -1,6 +1,7 @@
 import sh from 'shelljs'
 sh.cd("..");
 
-sh.exec(" npm exec --prefer-online @leothorp/npm-package-template@latest test-pkg")
-// sh.rm("-rf", "./test-pkg")
-sh.cd("./npm-package-template");
+sh.exec("npm exec --prefer-online @leothorp/npm-package-template@latest test-pkg")
+sh.rm("-rf", "./test-pkg")
+sh.cd("-");
+
