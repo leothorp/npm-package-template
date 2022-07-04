1a987a9378f82709ced959f9640a58aaec4b6868
leothorp <leothorp@users.noreply.github.com>
2022-07-04 14:19:39 -0500

    test

--- tests/run.js
+++ tests/run.js
@@ -2,5 +2,5 @@ import sh from 'shelljs'
 sh.cd("..");
 
 sh.exec("npx @leothorp/npm-package-template@latest test-pkg")
-sh.rm("-rf", "./test-pkg")
-shell.cd("./npm-package-template");
+// sh.rm("-rf", "./test-pkg")
+sh.cd("./npm-package-template");
